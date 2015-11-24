d1d1b27538c762e62435c194608254ccc41b8fef
Taliesin Sisson <taliesins@interxion.com>
2015-11-24 15:48:09 +0000

    Need to create directory parent and not actually directory itself as 7zip will only run if checksum directory does not exist Need to ensure folder exists. 7zip will only create 1 directory deep.
    Use a fixed length folder name
    Use checksum folder to install from

--- recipes/server.rb
+++ recipes/server.rb
@@ -79,11 +79,15 @@ remote_file download_path do
   only_if { is_iso }
 end
 
-iso_extraction_dir = "#{Chef::Config['file_cache_path']}/#{package_checksum}"
+directory "#{Chef::Config['file_cache_path']}/sql_server" do
+  recursive true
+end
+
+iso_extraction_dir = "#{Chef::Config['file_cache_path']}/sql_server/#{package_checksum}"
 
 execute 'extract_iso' do
   command "#{File.join(node['7-zip']['home'], '7z.exe')} x -y -o\"#{iso_extraction_dir}\" #{download_path}"
-  only_if { is_iso && !(::File.directory?(download_path)) }
+  only_if { is_iso && !(::File.directory?(iso_extraction_dir)) }
 end
 
 windows_package package_name do
