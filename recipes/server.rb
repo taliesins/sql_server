10c773fe261a9e03119306806697be49ed8edfee
Taliesin Sisson <taliesins@interxion.com>
2017-08-11 14:15:45 +0100

    Pass SAPWD via command line so that its not stored on the machine anywhere TCP_Enabled and NP_Enable are the attributes to use Allow .7z, .zip as well as iso for installation source

--- recipes/server.rb
+++ recipes/server.rb
@@ -20,20 +20,13 @@
 
 Chef::Application.fatal!("node['sql_server']['server_sa_password'] must be set for this cookbook to run") if node['sql_server']['server_sa_password'].nil?
 
-config_file_path = win_friendly_path(File.join(Chef::Config[:file_cache_path], 'ConfigurationFile.ini'))
-
 sql_sys_admin_list = if node['sql_server']['sysadmins'].is_a? Array
                        node['sql_server']['sysadmins'].map { |account| %("#{account}") }.join(' ') # surround each in quotes, space delimit list
                      else
                        %("#{node['sql_server']['sysadmins']}") # surround in quotes
                      end
 
-template config_file_path do
-  source 'ConfigurationFile.ini.erb'
-  variables(
-    sqlSysAdminList: sql_sys_admin_list
-  )
-end
+config_file_path = win_friendly_path(File.join(Chef::Config[:file_cache_path], 'ConfigurationFile.ini'))
 
 version = node['sql_server']['version']
 x86_64 = node['kernel']['machine'] =~ /x86_64/
@@ -53,6 +46,7 @@ package_checksum = node['sql_server']['server']['checksum'] ||
 # Build safe password command line options for the installer
 # see http://technet.microsoft.com/library/ms144259
 passwords_options = {
+  SAPWD: node['sql_server']['server_sa_password'],
   AGTSVCPASSWORD: node['sql_server']['agent_account_pwd'],
   RSSVCPASSWORD:  node['sql_server']['rs_account_pwd'],
   SQLSVCPASSWORD: node['sql_server']['sql_account_pwd'],
@@ -64,30 +58,37 @@ passwords_options = {
   enclosing_escape = safe_password.count('"').odd? ? '^' : ''
   "/#{option}=\"#{safe_password}#{enclosing_escape}\""
 end.compact.join ' '
-is_sqlserver_installed = is_package_installed?(package_name)
 
+is_sqlserver_installed = is_package_installed?(package_name)
 filename = File.basename(package_url).downcase
 fileextension = File.extname(filename)
-is_iso = ['.iso'].include? fileextension
+is_iso = ['.iso', '.7z', '.zip'].include? fileextension
+download_path = "#{Chef::Config['file_cache_path']}/#{filename}"
+iso_extraction_dir = "#{Chef::Config['file_cache_path']}/sql_server/#{package_checksum}"
 
 include_recipe 'seven_zip' if is_iso
 
-download_path = "#{Chef::Config['file_cache_path']}/#{filename}"
 remote_file download_path do
   source package_url
   checksum package_checksum
-  only_if { is_iso }
+  only_if { is_iso && !is_sqlserver_installed }
 end
 
 directory "#{Chef::Config['file_cache_path']}/sql_server" do
   recursive true
 end
 
-iso_extraction_dir = "#{Chef::Config['file_cache_path']}/sql_server/#{package_checksum}"
-
 execute 'extract_iso' do
   command "\"#{File.join(node['seven_zip']['home'], '7z.exe')}\" x -y -o\"#{iso_extraction_dir}\" #{download_path}"
-  only_if { is_iso && !(::File.directory?(iso_extraction_dir)) }
+  only_if { is_iso && !is_sqlserver_installed && !(::File.directory?(iso_extraction_dir)) }
+end
+
+template config_file_path do
+  source 'ConfigurationFile.ini.erb'
+  variables(
+    sqlSysAdminList: sql_sys_admin_list
+  )
+  only_if {!is_sqlserver_installed}
 end
 
 windows_package package_name do
@@ -110,11 +111,3 @@ reboot 'sql server install' do
 end
 
 include_recipe 'sql_server::configure'
-if iso_install
-  powershell_script 'Dismount #{filename}' do
-    code <<-EOH
-      Dismount-DiskImage -ImagePath "#{download_path}"
-      EOH
-    not_if {is_sqlserver_installed}
-  end
-end
